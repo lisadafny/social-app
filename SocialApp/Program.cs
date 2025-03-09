cabdddc490220c8e67865a52ccdd0607fd201e02
Lisa Dafny <lisadafny.tech@gmail.com>
2025-03-09 20:14:47 -0300

    feat: config database connection string

--- SocialApp/Program.cs
+++ SocialApp/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Database
+string dbConnString = builder.Configuration.GetConnectionString("Default");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
