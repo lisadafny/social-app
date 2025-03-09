0b8ac32b9b2fea5b7a54a69de26943fae23c6ce3
Lisa Dafny <lisadafny.tech@gmail.com>
2025-03-09 20:27:39 -0300

    feat: config entity framework

--- SocialApp/Program.cs
+++ SocialApp/Program.cs
@@ -1,10 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SocialApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//Database
+// Database config
 string dbConnString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(dbConnString));
 
 var app = builder.Build();
 
